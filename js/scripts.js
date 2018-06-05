58a6e696cfb14f4955a5c806461aad28581666ca
Matt Staudacher and Steven Colburn <matt@>
2018-06-05 09:53:26 -0700

    add campare function to retun true if the value is a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,30 @@
 $(function() {
   $("#btn-submit").click(function(event){
     event.preventDefault();
-
+    var sentence = $("#input").val().split("");
+    var originalSentence = $("#input").val();
+    for(var index = 0; index < sentence.length; index++)
+    {
+      if(compareTo(sentence[index]))
+      {
+        sentence[index] = "-";
+      }
+    }
+    $("#output").text("Output: ");
+    $("#output").append(sentence.join(""));
+    $("#input").val("");
+    $("#original").hide();
+    $("#original").text("Original: "+originalSentence);
   });
+
+  $("#btn-reveal").click(function(event){
+    event.preventDefault();
+    $("#original").slideDown();
+    });
+
 });
+
+function compareTo(value)
+{
+  return ((value==="a")||(value==="e")||(value==="i")||(value==="o")||(value==="u")||(value==="A")||(value==="E")||(value==="I")||(value==="O")||(value==="U"));
+}
